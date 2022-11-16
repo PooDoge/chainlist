3bd00210366f31195d020f26b22de43c3053d919
James Balducci <jimbalny@gmail.com>
2022-11-15 22:52:44 -0500

    test

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -32,12 +32,6 @@ export default function Layout({ changeTheme, theme, children }) {
               : classes.container
           }
         >
-          <div className={classes.copyContainer}>
-            <div className={classes.copyCentered}>
-
-
-            </div>
-          </div>
           <div
             className={
               theme.palette.type === "dark"
